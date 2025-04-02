189c6a0e8dc0f9892cdcc0770605f888d26f49be
HaiMinhDoan <haiminh17052003@gmail.com>
2025-04-02 10:49:14 +0700

    aaa

--- slm_admin/src/views/AddContent.vue
+++ slm_admin/src/views/AddContent.vue
@@ -40,8 +40,21 @@
                     </tr>
                     <tr>
                         <td>Nội dung chi tiết</td>
-                        <td>
-                            <Editor v-model="content" :init="editorConfig" api-key="bxb68lg12i11zhb0ei9ubbfw1t388posa12jg0hpmq8570dg"></Editor>
+                        <td style="color: black;">
+                            <div class="editor-container editor-container_document-editor editor-container_include-word-count"
+                                ref="editorContainerElement">
+                                <div class="editor-container__menu-bar" ref="editorMenuBarElement"></div>
+                                <div class="editor-container__toolbar" ref="editorToolbarElement"></div>
+                                <div class="editor-container__editor-wrapper">
+                                    <div class="editor-container__editor">
+                                        <div ref="editorElement">
+                                            <ckeditor v-if="editor && config" :modelValue="config.initialData"
+                                                :editor="editor" :config="config" @ready="onReady" v-model="content"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="editor_container__word-count" ref="editorWordCountElement"></div>
+                            </div>
                         </td>
                     </tr>
                     <tr>
@@ -78,7 +91,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>Thêm vật nội dung</td>
+                        <td>Thêm nội dung</td>
                         <td>
                             <button type="button" @click="createContent()">Tạo nội dung</button>
                         </td>
@@ -90,11 +103,238 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, useTemplateRef,computed } from 'vue'
 import { useRoute } from 'vue-router'
-import Editor from '@tinymce/tinymce-vue';
+import { Ckeditor } from '@ckeditor/ckeditor5-vue';
+import '../assets/style.css';
 // const CONST_HOST = "http://localhost:8080"
 const CONST_HOST = "https://id.slmsolar.com"
+
+import {
+	DecoupledEditor,
+	Alignment,
+	AutoLink,
+	Autosave,
+	BalloonToolbar,
+	Bold,
+	Code,
+	Essentials,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	Heading,
+	HorizontalLine,
+	ImageEditing,
+	ImageUtils,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	Paragraph,
+	RemoveFormat,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	TextPartLanguage,
+	Title,
+	Underline,
+	WordCount
+} from 'ckeditor5';
+
+import translations from 'ckeditor5/translations/vi.js';
+
+import 'ckeditor5/ckeditor5.css';
+
+const LICENSE_KEY =
+	'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDQ4NDc5OTksImp0aSI6IjRkNWRlZmY3LTc4OWMtNDNlNC1hZTBmLWMzYjk4NjgwYTM5YyIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjAyMWVhOGUxIn0.Q6Igx3cWrL7tETWI-rEwu2eWYTj7OOcKf8PZhn0PqPEkJDtzGGD71ReRvx21-Dm2F0C0qr-c2TrhHhblYQbPCA';
+
+const editorToolbar = useTemplateRef('editorToolbarElement');
+const editorMenuBar = useTemplateRef('editorMenuBarElement');
+const editorWordCount = useTemplateRef('editorWordCountElement');
+const isLayoutReady = ref(false);
+
+// function printContent(){
+//     console.log(content.value);
+    
+// }
+const editor = DecoupledEditor;
+
+const config = computed(() => {
+	if (!isLayoutReady.value) {
+		return null;
+	}
+
+	return {
+		toolbar: {
+			items: [
+				'textPartLanguage',
+				'|',
+				'heading',
+				'|',
+				'fontSize',
+				'fontFamily',
+				'fontColor',
+				'fontBackgroundColor',
+				'|',
+				'bold',
+				'italic',
+				'underline',
+				'strikethrough',
+				'subscript',
+				'superscript',
+				'code',
+				'removeFormat',
+				'|',
+				'horizontalLine',
+				'link',
+				'insertTable',
+				'|',
+				'alignment',
+				'|',
+				'outdent',
+				'indent'
+			],
+			shouldNotGroupWhenFull: false
+		},
+		plugins: [
+			Alignment,
+			AutoLink,
+			Autosave,
+			BalloonToolbar,
+			Bold,
+			Code,
+			Essentials,
+			FontBackgroundColor,
+			FontColor,
+			FontFamily,
+			FontSize,
+			Heading,
+			HorizontalLine,
+			ImageEditing,
+			ImageUtils,
+			Indent,
+			IndentBlock,
+			Italic,
+			Link,
+			Paragraph,
+			RemoveFormat,
+			Strikethrough,
+			Subscript,
+			Superscript,
+			Table,
+			TableCaption,
+			TableCellProperties,
+			TableColumnResize,
+			TableProperties,
+			TableToolbar,
+			TextPartLanguage,
+			Title,
+			Underline,
+			WordCount
+		],
+		balloonToolbar: ['bold', 'italic', '|', 'link'],
+		fontFamily: {
+			supportAllValues: true
+		},
+		fontSize: {
+			options: [10, 12, 14, 'default', 18, 20, 22],
+			supportAllValues: true
+		},
+		heading: {
+			options: [
+				{
+					model: 'paragraph',
+					title: 'Paragraph',
+					class: 'ck-heading_paragraph'
+				},
+				{
+					model: 'heading1',
+					view: 'h1',
+					title: 'Heading 1',
+					class: 'ck-heading_heading1'
+				},
+				{
+					model: 'heading2',
+					view: 'h2',
+					title: 'Heading 2',
+					class: 'ck-heading_heading2'
+				},
+				{
+					model: 'heading3',
+					view: 'h3',
+					title: 'Heading 3',
+					class: 'ck-heading_heading3'
+				},
+				{
+					model: 'heading4',
+					view: 'h4',
+					title: 'Heading 4',
+					class: 'ck-heading_heading4'
+				},
+				{
+					model: 'heading5',
+					view: 'h5',
+					title: 'Heading 5',
+					class: 'ck-heading_heading5'
+				},
+				{
+					model: 'heading6',
+					view: 'h6',
+					title: 'Heading 6',
+					class: 'ck-heading_heading6'
+				}
+			]
+		},
+		initialData:
+			'',
+		language: 'vi',
+		licenseKey: LICENSE_KEY,
+		link: {
+			addTargetToExternalLinks: true,
+			defaultProtocol: 'https://',
+			decorators: {
+				toggleDownloadable: {
+					mode: 'manual',
+					label: 'Downloadable',
+					attributes: {
+						download: 'file'
+					}
+				}
+			}
+		},
+		menuBar: {
+			isVisible: true
+		},
+		placeholder: 'Type or paste your content here!',
+		table: {
+			contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+		},
+		translations: [translations]
+	};
+});
+
+onMounted(() => {
+	isLayoutReady.value = true;
+});
+
+function onReady(editor) {
+	[...editorWordCount.value.children].forEach(child => child.remove());
+	[...editorToolbar.value.children].forEach(child => child.remove());
+	[...editorMenuBar.value.children].forEach(child => child.remove());
+
+	const wordCount = editor.plugins.get('WordCount');
+	editorWordCount.value.appendChild(wordCount.wordCountContainer);
+	editorToolbar.value.appendChild(editor.ui.view.toolbar.element);
+	editorMenuBar.value.appendChild(editor.ui.view.menuBarView.element);
+}
+
 
 const content_categories = ref([])
 const description = ref('')
@@ -110,15 +350,7 @@ const newMedia = ref({
     kind: 'image', // Mặc định là ảnh
     link: ''
 }); // Biến lưu thông tin media mới
-// Cấu hình TinyMCE
-const editorConfig = {
-    height: 500,
-    width: 1000,
-    menubar: true,
-    plugins: 'lists link image table code',
-    toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image table | code',
-    branding: false
-};
+
 // Hàm thêm media vào danh sách
 const addMedia = () => {
     if (newMedia.value.title.trim() !== '' && newMedia.value.link.trim() !== '') {
